dc7b365ca2c5d8f61f59af7517c1438cdd9a8907
Alexx <>
2021-04-24 23:08:27 -0400

    full project emoji

---
+++ Emoji/DefinitionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DefinitionViewController.swift
+//  Emoji
+//
+//  Created by Alejandro Diaz Sotolongo on 4/24/21.
+//  Copyright © 2021 aaaa. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    var emoji = "NO emojio"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print(emoji)
+    }
+    
+
+
+
+}
---
+++ Emoji/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Emoji
+//
+//  Created by Alejandro Diaz Sotolongo on 4/24/21.
+//  Copyright © 2021 aaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var Dacooltableview: UITableView!
+    
+    var emojis = ["😀", "😌", "😗", "🥰", "🤣"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        Dacooltableview.dataSource = self
+        Dacooltableview.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "moveSeque", sender: emoji)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let defVC = seque.destination as? DefinitionViewController
+        defVC.emoji = sender as! String
+        
+    }
+
+}
+
