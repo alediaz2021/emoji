0059d7e201924c6261e9d0d44cd7dbba1ab1facd
Alexx <>
2021-05-21 14:56:33 -0400

    qqqq

--- Emoji/ViewController.swift
+++ Emoji/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var Dacooltableview: UITableView!
 
 Jhjhjhjhjhjhjhjhhjhjh
+    cloneeeeeee
     
     var emojis = ["😀", "😌", "😗", "🥰", "🤣"]
     
